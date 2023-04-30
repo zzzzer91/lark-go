7ad018c7d6dbff1bf2c593ec14bd492c9c79e6e5
zzzzer <zzzzer91@gmail.com>
2023-04-30 15:40:09 +0800

    feat: add wiki api

--- service.go
+++ service.go
@@ -6,11 +6,13 @@ import (
 	lark_core "github.com/zzzzer91/lark-go/core"
 	lark_docx "github.com/zzzzer91/lark-go/service/docx/v1"
 	lark_im "github.com/zzzzer91/lark-go/service/im/v1"
+	lark_wiki "github.com/zzzzer91/lark-go/service/wiki/v2"
 )
 
 type LarkService interface {
 	lark_im.ImService
 	lark_docx.DocxService
+	lark_wiki.WikiService
 }
 
 func NewService(appID, appSecret string, timeout time.Duration) LarkService {
@@ -18,10 +20,12 @@ func NewService(appID, appSecret string, timeout time.Duration) LarkService {
 	return &larkServiceImpl{
 		ImService:   lark_im.NewService(cli),
 		DocxService: lark_docx.NewService(cli),
+		WikiService: lark_wiki.NewService(cli),
 	}
 }
 
 type larkServiceImpl struct {
 	lark_im.ImService
 	lark_docx.DocxService
+	lark_wiki.WikiService
 }
