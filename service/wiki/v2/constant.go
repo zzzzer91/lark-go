87fe2b2616d919e174aee04f6f03af7e8327c6f0
zzzzer <zzzzer91@gmail.com>
2023-04-30 16:25:43 +0800

    fix: wiki url

--- service/wiki/v2/constant.go
+++ service/wiki/v2/constant.go
@@ -3,5 +3,5 @@ package lark_wiki
 import lark_core "github.com/zzzzer91/lark-go/core"
 
 const (
-	urlWikiV2GetSpaceNodesTemplate = lark_core.UrlBasic + "/open-apis/wiki/v2/spaces/%s/nodes"
+	urlWikiV2GetSpaceNodesTemplate = lark_core.UrlBasic + "/wiki/v2/spaces/%s/nodes"
 )
